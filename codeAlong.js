fce58973f7b0c11dd577bc4ecf405cea680dcff2
Mandus Lindström <mandus85@hotmail.com>
2024-11-18 21:04:50 +0100

    Ready for codeAlong

---
+++ codeAlong.js
@@ -0,0 +1,12 @@
+console.log("hej tisdag");
+
+// Gör en GET-förfrågan med fetch() till JSONPlaceholder
+
+//   --------------------------------------------
+// Hantera fel i nätverksförfrågningar
+
+//   --------------------------------------------
+// Använd HTTPS för att hämta data från GitHub API
+
+// Hämta en användares publika repositories från GitHub API
+// --------------------------------------------
